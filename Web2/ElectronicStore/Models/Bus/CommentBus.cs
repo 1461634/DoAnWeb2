de2ecc67a24cb59306087df87e6223fff0273d7d
NguyenMinhQuang <minhquang1230@gmail.com>
2017-08-04 19:00:39 +0700

    comment

---
+++ Web2/ElectronicStore/Models/Bus/CommentBus.cs
@@ -0,0 +1,27 @@
+﻿using ElectronicStoreConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ElectronicStore.Models.Bus
+{
+    public class CommentBus
+    {
+        public static void Insert(Comment entity)
+        {
+            using (var db = new ElectronicStoreConnectionDB())
+            {
+                db.Insert(entity);
+            }
+        }
+
+        public static IEnumerable<Comment> ProductComments(int id)
+        {
+            using (var db = new ElectronicStoreConnectionDB())
+            {
+                return db.Query<Comment>("SELECT * FROM Comments WHERE Status = 1 AND ProductId = @0 ORDER BY DateCreated DESC", id);
+            }
+        }
+    }
+}
